cd8a56dc9a6af2a6049ad4f52534c0affa5f402a
Alex Shaw <alexjshaw@gmail.com>
2023-11-10 14:50:24 +0000

    trying to fix a layout bug

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,20 +1,17 @@
 /* eslint-disable react/display-name */
 import { forwardRef } from "react";
-import { Box, Center } from "@mantine/core";
+import { Box, Center, Container } from "@mantine/core";
 import { Helmet } from "react-helmet-async";
 import classes from "./PageLayout.module.css"
 
 const PageLayout = forwardRef(
   ({ children }, ref) => {
     return (
-      <Center ref={ref} className={classes.pageCenter}>
-        <Box className={classes.pageBox}>
+        <Container className={classes.pageContainer}>
+          <Center className={classes.pageCenter}>
           {children}
-        </Box>
-      </Center>
-        // <Box className={classes.pageBox}>
-        //   {children}
-        // </Box>
+          </Center>
+        </Container>
     );
   }
 );
